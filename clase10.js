4d3c63df2c91f5d05291eefb89b45703c2bba0ac
guillermomari <guillermo.a.mari@gmail.com>
2022-01-13 21:24:19 -0300

    Clase 13-1-22

---
+++ clase10.js
@@ -0,0 +1,44 @@
+// ASINCRONISMO - Async Await  y Callbacks
+/*
+async function levantarse(){
+    console.log('Estoy levantado')
+   await ponerlapava()
+   
+}
+ function ponerlapava(){
+    console.log('La pava esta puesta')
+    
+}
+
+
+
+function lavardientes(){
+    console.log('Los dientes estan lavados')
+}
+
+levantarse()
+
+*/
+
+// EJERCICIOS PRACTICOS DE ASINCRONIAS
+
+/**
+ * Crear una funcion asincronica que ejecute 
+ * 2 metodos de un objeto de manera sincronica 
+ * dentro de su argumento 
+ * 
+ */
+
+
+/***
+ * A partir de una funcion asincronica 
+ * ejectur un prompt que ingrese numeros (3 casos)
+ * Luego una vez ejecutada la funcion sincronica que ingresa 
+ * dichos numeros, guardarlos en un array y mostrarlos
+ * con otra funcion asinconrica( await ) que se encargue 
+ * de hacer el console log de cada elemento ingresado al array
+ * 
+ * 
+ */
+
+
